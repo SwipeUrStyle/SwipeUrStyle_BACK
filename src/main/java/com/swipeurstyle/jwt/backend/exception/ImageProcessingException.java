9de150bc4030d08018dc7a404e1731be31e02aeb
Juliana Briceño <bricenojuliana@gmail.com>
2024-05-21 22:18:36 -0500

    SonarCloud issue: insecure configuration

---
+++ src/main/java/com/swipeurstyle/jwt/backend/exception/ImageProcessingException.java
@@ -0,0 +1,9 @@
+package com.swipeurstyle.jwt.backend.exception;
+
+
+public class ImageProcessingException extends Exception {
+    public ImageProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
