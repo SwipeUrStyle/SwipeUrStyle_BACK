645937add9ca85ef16981202c98432f4808bd403
Juliana Briceño <bricenojuliana@gmail.com>
2024-05-22 22:33:04 -0500

    SonarCloud issues

--- src/test/java/com/swipeurstyle/jwt/backend/service/OutfitServiceTest.java
+++ src/test/java/com/swipeurstyle/jwt/backend/service/OutfitServiceTest.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class OutfitServiceTest {
+class OutfitServiceTest {
 
     @Mock
     private OutfitRepository outfitRepositoryMock;
